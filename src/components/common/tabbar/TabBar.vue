5aa4c1f58d0fa7e05062936430b0f5b12a454b49
wzk1583456343 <1583456343@qq.com>
2021-07-04 23:24:27 +0800

    second

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id="tab-bar">
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar'
+}
+</script>
+
+<style>
+#tab-bar{
+  background-color: #f6f6f6;
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  box-shadow: 0 -1px 5px rgba(73, 73, 73, 0.15);
+}
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div><slot name="itemText"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  computed: {
+    isActive(){
+      return this.$route.path == this.path
+    }
+  },
+  methods: {
+    itemClick(){
+      if(this.$route.path != this.path)
+        this.$router.push(this.path)
+    }
+  },
+  props: {
+    path: String
+  }
+}
+</script>
+
+<style>
+.tab-bar-item{
+  flex: 1;
+  height: 49px;
+}
+img{
+  width: 22px;
+  vertical-align: bottom;
+  margin-top: 5px;
+  margin-bottom: 3px;
+}
+</style>
